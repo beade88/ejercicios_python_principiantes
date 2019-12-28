991fd781c8db2c6702e95f5b2cb0c9e76339a2e9
Beade <beade88@gmail.com>
2019-12-28 04:42:26 -0500

    Importante...en este ejercicio ya transforme el codigo de python2 a python3, las dos diferencias que generaban error al compilar el codigo hecho con python2 con python3 fueron que ahora en python 3 print es una funcion y hay que ponerle () y lo otro fue que en python3 input es una cadena de texto, para poder usar los numeros tuve que convertir con int antes de input

--- ej2.py
+++ ej2.py
@@ -1,4 +1,4 @@
-n = input ("Ingrese un numero: ")
+n = int (input ("Ingrese un numero: "))
 res = n % 2 
 res2 = n % 4
 if res > 0 : 
@@ -7,13 +7,14 @@ else :
 	print ('Usted ha ingresado un numero par')
 if res2 % 4 == 0 :
 	print ('Su numero es multiplo de 4')
-print "__________________________________________________"
-print "Ahora comprobemos si dos numeros son divisibles :D"
-chek = input ("Ingrese el primer numero (dividendo): ")
-num = input ("Ingrese el segundo numero (divisor): ")
+print ("__________________________________________________")
+print ("Ahora comprobemos si dos numeros son divisibles :D")
+chek = int (input ("Ingrese el primer numero (dividendo): "))
+num = int (input ("Ingrese el segundo numero (divisor): "))
 x = chek % num
 if x > 0 :
-	print 'El numero ', chek, 'no es divisible por ', num
+	print ('El numero ', chek, 'no es divisible por ', num)
 else : 
-	print 'El numero ', chek, 'es divisible por ', num
+	print ('El numero ', chek, 'es divisible por ', num)
+	print ("Importante...en este ejercicio ya transforme el codigo de python2 a python3, las dos diferencias que generaban error al compilar el codigo hecho con python2 con python3 fueron que ahora en python 3 print es una funcion y hay que ponerle () y lo otro fue que en python3 input es una cadena de texto, para poder usar los numeros tuve que convertir con int antes de input")
 	
