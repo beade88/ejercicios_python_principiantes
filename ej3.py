251204bbd649305739697d88061a7c3f70ef667f
Beade <beade88@gmail.com>
2019-12-28 16:46:29 -0500

    I add the exercise description.

--- ej3.py
+++ ej3.py
@@ -1,3 +1,18 @@
+'''
+Exercise 3
+
+Take a list, say for example this one:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+and write a program that prints out all the elements of the list that are less than 5.
+
+Extras:
+
+    Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+    Write this in one line of Python.
+    Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+'''
 print "Vamos a elaborar una lista. Para ello le pediremos que ingrese 10 numeros"
 print "De los numeros ingresados le mostraremos los menores que 5"
 a = input ("Ingrese un numero: ")
