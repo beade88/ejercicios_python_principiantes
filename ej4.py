4a4310d0a1e2df28e1d3d403380a56f9e7b1887d
Beade <beade88@gmail.com>
2019-12-28 16:47:02 -0500

    I add the exercise description.

--- ej4.py
+++ ej4.py
@@ -1,3 +1,7 @@
+'''
+Exercise 4
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+'''
 print "En el siguiente programa le solicitaremos que ingrese un numero y luego le mostraremos los numeros que son divisibles por el numero ingresado"
 a = int(input("Ingrese un numero: "))
 my_list = range(1, a+1)
