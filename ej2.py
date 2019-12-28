21a75b2c6e1f54f1383aa4679b93d0aa44bee9ca
Beade <beade88@gmail.com>
2019-12-28 16:46:03 -0500

    I add the exercise description.

--- ej2.py
+++ ej2.py
@@ -1,3 +1,13 @@
+'''
+Exercise 2
+
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+    If the number is a multiple of 4, print out a different message.
+    Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+'''
 n = int (input ("Ingrese un numero: "))
 res = n % 2 
 res2 = n % 4
@@ -16,5 +26,7 @@ if x > 0 :
 	print ('El numero ', chek, 'no es divisible por ', num)
 else : 
 	print ('El numero ', chek, 'es divisible por ', num)
-	print ("Importante...en este ejercicio ya transforme el codigo de python2 a python3, las dos diferencias que generaban error al compilar el codigo hecho con python2 con python3 fueron que ahora en python 3 print es una funcion y hay que ponerle () y lo otro fue que en python3 input es una cadena de texto, para poder usar los numeros tuve que convertir con int antes de input")
+'''
+Importante...en este ejercicio ya transforme el codigo de python2 a python3, las dos diferencias que generaban error al compilar el codigo hecho con python2 con python3 fueron que ahora en python 3 print es una funcion y hay que ponerle () y lo otro fue que en python3 input es una cadena de texto, para poder usar los numeros tuve que convertir con int antes de input")
+'''
 	
