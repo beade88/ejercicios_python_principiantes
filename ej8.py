2fd1e09a547d43ae63c5d9d872e031fd0e239d51
Reinaldo Bloopark <reinaldo@bloopark.de>
2019-12-31 02:23:20 -0500

    [DEAL] But before the deal a BIG CONGRATULATIONS!!!

--- ej8.py
+++ ej8.py
@@ -1,11 +1,24 @@
 '''
 Exercise 8
-Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+Make a two-player Rock-Paper-Scissors game.
+(Hint: Ask for player plays (using input), compare them, print out a message
+of congratulations to the winner, and ask if the players want to start a new game)
 Remember the rules:
     Rock beats scissors
     Scissors beats paper
     Paper beats rock
 '''
+
+# Very Very Very good.
+# No PEP8 error, no spanish words, no english errors,
+# good variables naming...no comments here :D
+
+# But, as nothing is perfect, come on, lets thing another solution :D
+# I dont know, I let it open to your imagination :D
+# I just dont like to many validations here :D (I mean there are 9
+# if - else sentences because the binary combinations)
+# It should exist another way (I dont know exactly how to do it) :D
+# But I will think about it and let you know :D
 print('Lets to play Rock Paper Scissors')
 print('R means Rock, P means Paper and S means Scissors')
 game = 'y'
