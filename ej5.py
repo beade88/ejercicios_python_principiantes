b390d417c7350c6a35feb9f849a7f8252f56984a
Reinaldo Bloopark <reinaldo@bloopark.de>
2019-12-31 01:44:26 -0500

    [DEAL] Remake using foor loop and PEP8

--- ej5.py
+++ ej5.py
@@ -6,14 +6,23 @@ Take two lists, say for example these two:
   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+and write a program that returns a list that contains only the elements that are
+common between the lists (without duplicates). Make sure your program works on two
+lists of different sizes.
 
 Extras:
 
     Randomly generate two lists to test this
-    Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+    Write this in one line of Python (dont worry if you cant figure this
+    out at this point - we will get to it soon)
 '''
 #Este esta implementado con python3 desde el inicio :D
+
+# ******************************************************************
+# Please remake this excercise using a for loop to fill the lists
+# then push again
+# ******************************************************************
+
 print("Lista 1")
 a = int(input("Ingrese un numero: "))
 b = int(input("Ingrese un numero: "))
@@ -35,7 +44,10 @@ print("La lista 2 es: ")
 print(lista2)
 lista3 = []
 print("Los numeros que coinciden en ambas listas son: ")
-for x in lista2 :
-	if x in lista1 :
+for x in lista2 : # PEP8 here, lista2:
+	if x in lista1 : # PEP8 here, lista1:
 		lista3.append(x)
 print(lista3)
+
+# Remember, in PEP8 andy sign (, or :) goes without space before.
+# Any sign like (*, +, -) goes with spaces before and after
