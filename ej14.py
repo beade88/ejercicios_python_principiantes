5de891dcf43e127dce3605532c79995952056a6b
Beade <beade88@gmail.com>
2020-01-05 11:46:33 -0500

    [NEW] I created a function that returns a list without duplicated elements of tha an initial list.

---
+++ ej14.py
@@ -0,0 +1,19 @@
+"""
+Exercise 14
+Write a program (function!) that takes a list
+and returns a new list that contains all the
+elements of the first list minus all the duplicates.
+"""
+
+
+# This function returns  a list that contains the elements of the initial list but without duplicates elements.
+def list_without_duplicates(list_to_analyze):
+    return set(my_list)
+
+
+list_length = int(input('Type a length of your list: '))
+my_list = []
+for i in range(0, list_length):
+    my_list.append(int(input('Type a number: ')))
+
+print(list_without_duplicates(my_list))
