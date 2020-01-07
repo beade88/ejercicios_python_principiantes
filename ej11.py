772aca24dcd737900e1850443f529b0bb7813447
Reinaldo Bloopark <reinaldo@bloopark.de>
2020-01-06 23:29:51 -0500

    [PEP8] Read comment

--- ej11.py
+++ ej11.py
@@ -10,6 +10,18 @@ to practice using functions, described below.
 
 
 def prime_number(a):  # Defining a function that return if one number is prime or not
+    """The rigth PEP8 way is this.
+
+    Using two lines literal description (I dont remember the right name)
+    It says that you should always do it. When you declare a function you add
+    a description like this. Describing what the function does, and what the
+    parameters are for. The first line is like a tittle, and it should be short
+    And then you add this description. The blank line is also important between
+    the tittle and this description :D. It could seem like a 'boberia' but is
+    very useful. For instance, using pycharm and other IDE, when you type
+    the name of the function and you put the mouse arrow over you will see this
+    description. Test it, put the mouse over the name in line 40 and you see :D
+    """
     if a == 0:
         is_prime = False
     if a == 1:
