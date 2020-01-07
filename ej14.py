51a26bd8edbee9fe6bd47ee315cb2be70a828f18
Beade <beade88@gmail.com>
2020-01-07 00:12:41 -0500

    [OK] I saw all of your comments

--- ej14.py
+++ ej14.py
@@ -2,19 +2,17 @@
 Exercise 14
 Write a program (function!) that takes a list
 and returns a new list that contains all the
-elements of the first list minus all the duplicates.
+elements of the first list minus all the duplicates...
 """
 
 
 # This function returns  a list that contains the elements of the initial list but without duplicates elements.
 def list_without_duplicates(list_to_analyze):
-<<<<<<< HEAD
     return set(list_to_analyze)
-=======
+
     # you should return using list_to_analyze,
     # this is the name of the param
     return set(my_list)
->>>>>>> 77e1ea9ca4590ec9fe789050adbf69fc74820c05
 
 
 list_length = int(input('Type a length of your list: '))
